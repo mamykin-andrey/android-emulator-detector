003cf09ba9198bb7e110473b9cf92945bebd8456
Mamykin Andrey <mamykin.av@bk.ru>
2022-05-16 10:33:01 +0300

    Rename app module to sample

--- sample/src/main/java/ru/mamykin/emulatordetector/SampleActivity.kt
+++ sample/src/main/java/ru/mamykin/emulatordetector/SampleActivity.kt
@@ -1,9 +1,10 @@
 package ru.mamykin.emulatordetector
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SampleActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
